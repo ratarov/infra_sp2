673e172005ee34e3ce802c9ca5e37266b2403212
Ruslan Atarov <ruslanatarov@gmail.com>
2023-04-17 12:33:38 +0300

    added dockerhub

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -104,7 +104,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # OTHER SETTINGS
